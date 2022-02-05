72692d6518f7cf9c83243454819eba0076de1e7e
olamileke <olamileke.f@gmail.com>
2022-02-05 19:47:48 +0100

    feat: set return message on index route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(): string {
+    return 'Idk what you expected to find. I am an API.';
   }
 }
