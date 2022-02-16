804dd8573ab6c60a25b6cb1fbdcfbf06ee9f475c
olamileke <olamileke.f@gmail.com>
2022-02-16 20:14:39 +0100

    feat: continuous deployment test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { UnguardedRoute } from './utilities';
 
 @Controller()
 export class AppController {
   constructor() {}
 
+  @UnguardedRoute()
   @Get()
   index(): string {
     return 'Idk what you expected to find. I am an API. Continuous Deployment baby.';
